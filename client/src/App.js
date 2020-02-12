f78863bd96928a0bd200c202a7edc92873becb79
sbkim111 <sbkim111@daum.net>
2020-02-12 11:01:50 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
-import TableRew from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import { withStyles } from '@material-ui/core/styles';
@@ -13,42 +12,28 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
   root: {
     width: '100',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto"
   },
   table: {
     minWidth: 1080
   }
-})
-
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '홍길동1',
-  'birthday': '8101111',
-  'gender': '남자',
-  'job': '직장인'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',    
-    'name': '홍길동2',
-    'birthday': '81012222',
-    'gender': '여자',
-    'job': '주부'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',    
-    'name': '홍길동3',
-    'birthday': '81013333',
-    'gender': '남자',
-    'job': '건설'
-  }  
-]
+});
 
 class App extends Component {
+  state = {
+    customers: ""
+  }
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render(){
     const { classes } = this.props;
     return (
@@ -64,22 +49,10 @@ class App extends Component {
               <TableCell>직업</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody>
-            {
-              customers.map(c => {
-                return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />    
-                );
-              })
-            }
+          <TableBody>          
+            {this.state.customers ? this.state.customers.map(c => {
+                return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+              }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -4,10 +4,35 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.unsubscribe(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '홍길동1',
+        'birthday': '8101111',
+        'gender': '남자',
+        'job': '직장인'
+        },
+        {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',    
+        'name': '홍길동2',
+        'birthday': '81012222',
+        'gender': '여자',
+        'job': '주부'
+        },
+        {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',    
+        'name': '홍길동3',
+        'birthday': '81013333',
+        'gender': '남자',
+        'job': '건설'
+        }  
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
